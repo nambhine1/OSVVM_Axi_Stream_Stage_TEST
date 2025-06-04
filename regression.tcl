34eb5f0c970a4ac4291ac96d64a9ef79fcbb0d59
rakotojaona <53557693+nambhine1@users.noreply.github.com>
2025-06-04 13:53:23 +0200

    update tcl script

--- regression.tcl
+++ regression.tcl
@@ -1,26 +1,27 @@
 source lib/OsvvmLibraries/Scripts/StartUp.tcl
+variable OMIT_XILINX_FILES 0
 
 build lib/OsvvmLibraries/OsvvmLibraries.pro
       
-variable OMIT_XILINX_FILES 1
-      
 if {$::osvvm::ToolName eq "GHDL"} {
+    set OMIT_XILINX_FILES 1
     SetExtendedAnalyzeOptions {-frelaxed -Wno-specs}
     SetExtendedSimulateOptions {-frelaxed -Wno-specs -Wno-binding}
-	SetExtendedRunOptions {--ieee-asserts=disable-at-0}
+    SetExtendedRunOptions {--ieee-asserts=disable-at-0}
 }
       
 if {$::osvvm::ToolName eq "RivieraPRO"} {
+    set OMIT_XILINX_FILES 1
     SetExtendedSimulationOptions {-unbounderror}
     LinkLibraryDirectory {temp/Riviera-PRO/VHDL_LIBS/*}
 }
 
 if {$::osvvm::ToolName eq "QuestaSim"} {
+        set OMIT_XILINX_FILES 0
 	SetVHDLVersion 2008
 	vmap unisim "$PrecompiledVivadoIPCores/unisim"
 	vmap xpm "$PrecompiledVivadoIPCores/xpm"
 }
-
 
 build src/passthrough.pro
 
